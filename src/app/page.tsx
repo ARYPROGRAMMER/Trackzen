0603e736dab9478320488a7db336d6ac4e043e3f
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-06 03:53:03 +0530

    customisation finished
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select } from "@/components/ui/select";
 
 export default function Home() {
   return (
     <div className="">
+      <Select />
+      <Input />
       <Button variant={"primary"} size={"xs"}>Primary</Button>
       <Button variant={"secondary"}>Secondary</Button>
       <Button variant={"destructive"}>Destructive</Button>
