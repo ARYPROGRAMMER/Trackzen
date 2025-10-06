690fe8a2e7555bcf0e812e1aece023feb207da3d
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-07 02:51:18 +0530

    setting up auth with appwrite p1
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
+import { SignInCard } from '@/features/auth/components/sign-in-card'
 import React from 'react'
 
 function SignInPage() {
   return (
-    <div>SignInPage</div>
+    <SignInCard />
   )
 }
 
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+
+
+import { SignUpCard } from '@/features/auth/components/sign-up-card';
 import React from 'react'
 
 function SignUpPage() {
   return (
-    <div>SignUpPage</div>
+    <SignUpCard />
   )
 }
 
