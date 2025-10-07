5653ca4536512ae9e7b5081e59375ca0508cbd81
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-07 17:16:34 +0530

    type definition and rpc structuring for hono
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -15,22 +15,20 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import Link from "next/link";
+import { loginSchema } from "../schemas";
 
-const formSchema = z.object({
-  email: z.email(),
-  password: z.string().min(1, "Password is required"),
-});
+
 
 export const SignInCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
     console.log({ values });
   };
 
