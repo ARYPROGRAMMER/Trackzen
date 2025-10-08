ecd37c956b841fb4c3fbad3111b642adfd4d17b4
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-09 02:52:53 +0530

    fetch workspace
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,13 +1,36 @@
 "use client";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspace";
+import { RiAddCircleFill } from "react-icons/ri";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 export const WorkspaceSwitcher = () => {
+  const { data: workspaces } = useGetWorkspaces();
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Workspaces</p>
+        <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
+      </div>
 
-    const {data} = useGetWorkspaces();
-    return (
-        <div>
-            {data?.total}
-        </div>
-    )
-}
+      <Select>
+        <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
+          <SelectValue placeholder="No workspace selected" />
+        </SelectTrigger>
+        <SelectContent>
+          {workspaces?.rows.map((workspace) => (
+            <SelectItem key={workspace.id} value={workspace.$id}>
+              {workspace.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
