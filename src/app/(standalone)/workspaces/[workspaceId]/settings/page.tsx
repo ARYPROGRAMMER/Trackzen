6e8d65494386630d5d2ae175f70039b4de7c4360
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-13 06:25:52 +0530

    inferance settings page
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,31 +1,35 @@
-import { getCurrent } from '@/features/auth/actions'
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { redirect } from "next/navigation";
+import React from "react";
 
 interface WorkspaceIdSettingsPageProps {
-    params: {
-        workspaceId: string
-    }
-
+  params: {
+    workspaceId: string;
+  };
 }
 
+const WorkspaceIdSettingsPage = async ({
+  params,
+}: WorkspaceIdSettingsPageProps) => {
+  const user = await getCurrent();
 
+  if (!user) {
+    redirect("/sign-in");
+  }
 
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
 
-const WorkspaceIdSettingsPage = async (
-    { params }: WorkspaceIdSettingsPageProps
-) => {
-    const user = await getCurrent();
-
-    if (!user) {
-        redirect("/sign-in");
-    }
+  if (!initialValues) {
+    redirect(`/workspaces/${params.workspaceId}`);
+  }
 
   return (
     <div>
-      WorkspaceIdSettingsPage: {params.workspaceId}
+      <EditWorkspaceForm initialValues={initialValues} />
     </div>
-  )
-}
+  );
+};
 
-export default WorkspaceIdSettingsPage
+export default WorkspaceIdSettingsPage;
--- src/features/workspaces/actions.ts
+++ src/features/workspaces/actions.ts
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import { Account, Client, Query, TablesDB } from "node-appwrite";
 import { AUTH_COOKIE } from "../auth/constants";
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { getMember } from "../members/utils";
 
 export const getWorkspaces = async () => {
   try {
@@ -36,5 +37,47 @@ export const getWorkspaces = async () => {
     return workspaces;
   } catch {
     return { rows: [], total: 0 };
+  }
+};
+
+interface GetWorkspaceProps {
+  workspaceId: string;
+}
+
+export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
+  try {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = await cookies().get(AUTH_COOKIE);
+
+    if (!session) return null;
+
+    client.setSession(session.value);
+
+    const tables = new TablesDB(client);
+    const account = new Account(client);
+    const user = await account.get();
+
+    const member = await getMember({
+      tables,
+      userId: user.$id,
+      workspaceId,
+    });
+
+    if (!member) {
+      return null;
+    }
+
+    const workspace = (await tables.getRow(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId
+    )) as any;
+
+    return workspace;
+  } catch {
+    return null;
   }
 };
