6ab3b362e0a51910aded40af3d55cb51d52a2337
Arya Pratap Singh <notaryasingh@gmail.com>
2025-11-18 18:21:40 +0530

    post/get routes for tasks
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

---
+++ src/features/tasks/server/route.ts
@@ -0,0 +1,176 @@
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { createTaskSchema } from "../schemas";
+import { getMember } from "@/features/members/utils";
+import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import z from "zod";
+import { TaskStatus } from "../types";
+import { createAdminClient } from "@/lib/appwrite";
+
+const app = new Hono()
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("json", createTaskSchema),
+    async (c) => {
+      const user = c.get("user");
+      const tables = c.get("tables");
+      const { name, status, assigneeId, dueDate, projectId, workspaceId } =
+        c.req.valid("json");
+
+      const member = await getMember({
+        tables,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json(
+          {
+            error: "Unauthorized",
+          },
+          401
+        );
+      }
+
+      const highestPositionTask = await tables.listRows(DATABASE_ID, TASKS_ID, [
+        Query.equal("status", status),
+        Query.equal("workspaceId", workspaceId),
+        Query.orderAsc("position"),
+        Query.limit(1),
+      ]);
+
+      const newPosition =
+        highestPositionTask.rows.length > 0
+          ? highestPositionTask.rows[0].position + 1000
+          : 1000;
+
+      const task = await tables.createRow(DATABASE_ID, TASKS_ID, ID.unique(), {
+        name,
+        status,
+        workspaceId,
+        projectId,
+        dueDate,
+        assigneeId,
+        position: newPosition,
+      });
+
+      return c.json({ data: task });
+    }
+  )
+
+  .get(
+    "/",
+    sessionMiddleware,
+    zValidator(
+      "query",
+      z.object({
+        workspaceId: z.string(),
+        projectId: z.string().nullish(),
+        assigneeId: z.string().nullish(),
+        status: z.enum(TaskStatus).nullish(),
+        search: z.string().nullish(),
+        dueDate: z.string().nullish(),
+      })
+    ),
+    async (c) => {
+      const { users } = await createAdminClient();
+
+      const tables = c.get("tables");
+      const user = c.get("user");
+      const { workspaceId, projectId, assigneeId, status, search, dueDate } =
+        c.req.valid("query");
+
+      const member = await getMember({
+        tables,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json(
+          {
+            error: "Unauthorized",
+          },
+          401
+        );
+      }
+
+      const query = [
+        Query.equal("workspaceId", workspaceId),
+        Query.orderDesc("$createdAt"),
+      ];
+
+      if (projectId) {
+        console.log("projectId: ", projectId);
+        query.push(Query.equal("projectId", projectId));
+      }
+
+      if (status) {
+        console.log("status: ", status);
+        query.push(Query.equal("status", status));
+      }
+      if (assigneeId) {
+        console.log("assigneeId: ", assigneeId);
+        query.push(Query.equal("assigneeId", assigneeId));
+      }
+      if (search) {
+        console.log("search: ", search);
+        query.push(Query.search("name", search));
+      }
+      if (dueDate) {
+        console.log("dueDate: ", dueDate);
+        query.push(Query.equal("dueDate", dueDate));
+      }
+
+      const tasks = await tables.listRows(DATABASE_ID, TASKS_ID, query);
+
+      const projectIds = tasks.rows.map((task) => task.projectId);
+      const assigneeIds = tasks.rows.map((task) => task.assigneeId);
+
+      const projects = await tables.listRows<any>(
+        DATABASE_ID,
+        PROJECTS_ID,
+        projectIds.length > 0 ? [Query.contains("$id", projectIds)] : []
+      );
+
+      const members = await tables.listRows(
+        DATABASE_ID,
+        MEMBERS_ID,
+        assigneeIds.length > 0 ? [Query.contains("$id", assigneeIds)] : []
+      );
+
+      const assignees = await Promise.all(
+        members.rows.map(async (member) => {
+          const user = await users.get(member.userId);
+
+          return {
+            ...member,
+            name: user.name,
+            email: user.email,
+          };
+        })
+      );
+
+      const populatedTasks = tasks.rows.map((task) => {
+        const project = projects.rows.find((p) => p.$id === task.projectId);
+        const assignee = assignees.find((a) => a.$id === task.assigneeId);
+        return {
+          ...task,
+          project,
+          assignee,
+        };
+      });
+
+      return c.json({
+        data: {
+          ...tasks,
+          rows: populatedTasks,
+        },
+      });
+    }
+  );
+
+export default app;
