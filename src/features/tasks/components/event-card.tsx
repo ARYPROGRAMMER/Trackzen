507a95e573eaf331bdfa2c452f77dda40c7f76e6
Arya Pratap Singh <notaryasingh@gmail.com>
2025-11-28 14:40:41 +0530

    ui elements for rbc
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -1,6 +1,8 @@
 import { Project } from "@/features/projects/types";
 import { TaskStatus } from "../types";
 import { cn } from "@/lib/utils";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 interface EventCardProps {
   title: string;
@@ -9,6 +11,14 @@ interface EventCardProps {
   status: TaskStatus;
   id: string;
 }
+
+const statusColorMap: Record<TaskStatus, string> = {
+  [TaskStatus.TODO]: "border-l-red-500",
+  [TaskStatus.IN_PROGRESS]: "border-l-yellow-500",
+  [TaskStatus.DONE]: "border-l-emerald-500",
+  [TaskStatus.BACKLOG]: "border-l-pink-500",
+  [TaskStatus.IN_REVIEW]: "border-l-blue-500",
+};
 
 export const EventCard = ({
   title,
@@ -21,10 +31,17 @@ export const EventCard = ({
     <div className="px-2">
       <div
         className={cn(
-          "p-1.5 text-xs text-primary bg-white border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition"
+          "p-1.5 text-xs text-primary bg-white border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition",
+          statusColorMap[status]
         )}
       >
         <p>{title}</p>
+        <div className="flex items-center gap-x-1">
+          <MemberAvatar name={assignee?.name} />
+          <div className="size-1 rounded-full bg-neutral-300" />
+
+          <ProjectAvatar name={project?.name} image={project?.imageUrl} />
+        </div>
       </div>
     </div>
   );
